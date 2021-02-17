b00d3477c80e56c8f1e7edb544e03934a93b570e
Nigel Gott <nigel.gott@gmail.com>
2021-02-17 10:14:07 +0000

    Fix issues with tests running using docker clashing with an already existing local dev run of the application.

--- tests.sh
+++ tests.sh
@@ -4,6 +4,6 @@ IFS=$'\n\t'
 
 trap "docker-compose -f test.yml logs && docker-compose -f test.yml down && docker-compose -f test.yml rm" EXIT
 docker-compose -f test.yml build
-docker-compose -f test.yml run --rm --entrypoint pytest django
+docker-compose -f test.yml run --rm --entrypoint pytest django_test
 docker-compose -f test.yml down
 docker-compose -f test.yml rm
